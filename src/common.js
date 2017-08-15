a3546b06008737587760496b53a13e28a6a8b036
Alex Quot <alexquot@gmail.com>
2017-08-15 10:57:04 -0400

    Add logging features, some default config options

--- src/common.js
+++ src/common.js
@@ -1,10 +1,49 @@
-const config = {
-  baseUrl: 'https://google.com',
-  shouldCapture: true
+const common = {
+
+  config: {
+    baseUrl: 'http://192.168.100.196:8280/cloud',
+    shouldCapture: true,
+    shouldLog: {
+      clientLogs: false,
+      clientErrors: true
+    }
+  },
+
+  capture: function (prefix, suffix) {
+    if (this.config.shouldCapture) {
+      casper.capture(
+        'captured/' +
+        (prefix || '') +
+        Date.now() +
+        (suffix || '') +
+        '.png'
+      )
+    }
+  },
+
+  dump: function (prefix, suffix) {
+    require('fs').write(
+      prefix + 'dump' + suffix + '.html',
+      casper.getHTML(),
+      'w'
+    )
+  }
+
 }
 
-function capture () {
-  if (config.shouldCapture) {
-    casper.capture('captured/' + Date.now() + '.png')
-  }
+//
+//
+//
+
+if (common.config.shouldLog.clientLogs) {
+  casper.on('remote.message', function (message) {
+    this.echo(message)
+  })
+}
+
+if (common.config.shouldLog.clientErrors) {
+  casper.on('page.error', function (msg, trace) {
+    this.echo(msg, 'ERROR')
+    this.echo(JSON.stringify(trace), 'WARNING')
+  })
 }
